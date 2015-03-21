c185f99d33b266b70aaabc2e00f9b6241eb2bda5
hfaran <hamzafaran@outlook.com>
2015-03-21 01:05:44 -0700

    setUp -> __init__; self.c -> self.cursor

--- sqlAPI.py
+++ sqlAPI.py
@@ -3,34 +3,33 @@ import sqlite3
 
 class SQLAPI(object):
 
-    def tableCreate(self):
-        """
-        Note: This function is creates the tables and stuff but does not properly set the foreign and primary keys.
-        We only need to do this one and therefore we can do it using SQL manager once and use it for the rest of time
-        So use this for testing only.
-        """
-        self.c.execute("CREATE TABLE Employees (sin TEXT, fName TEXT, lName TEXT, username TEXT,password TEXT,salt TEXT)")
-        self.c.execute("CREATE TABLE Working  (sin TEXT, startShift TEXT, endShift TEXT)")
-        self.c.execute("CREATE TABLE Venue   (name TEXT, address TEXT)")
-        self.c.execute("CREATE TABLE Sport (name TEXT, sportID INT)")
-        # Note that I am using the start and end time as string. we initially had it as int.
-        self.c.execute("CREATE TABLE Session  (start_time TEXT, end_time TEXT, session_id UNSIGNED INT, sport_id UNSIGNED INT, venue_name TEXT, results TEXT)")
-        self.c.execute("CREATE TABLE Player (name TEXT , student_num UNSIGNED INT, password TEXT, salt TEXT)")
-        self.c.execute("CREATE TABLE PlaysIn (student_num UNSIGNED INT, team_id UNSIGNED INT)")
-        self.c.execute("CREATE TABLE Team_ParticipatesIn (name TEXT, team_ID UNSIGNED INT, number_of_players UNSIGNED INT,"
-                       "sport_id UNSIGNED INT, session_id INT,venue_name TEXT)")
-
-    def setUp(self, DBaddress='project.db'):
+    def __init__(self, DBaddress='project.db'):
         """
         input : String containing address of the database
         output: the cursor for that database for later use
         """
         if isinstance(DBaddress,str):
             self.conn = sqlite3.connect(DBaddress)
-            self.c = self.conn.cursor()
-            return self.c
+            self.cursor = self.conn.cursor()
         else :
             print ("Address of the database is not string/valid")
+
+    def tableCreate(self):
+        """
+        Note: This function is creates the tables and stuff but does not properly set the foreign and primary keys.
+        We only need to do this one and therefore we can do it using SQL manager once and use it for the rest of time
+        So use this for testing only.
+        """
+        self.cursor.execute("CREATE TABLE Employees (sin TEXT, fName TEXT, lName TEXT, username TEXT,password TEXT,salt TEXT)")
+        self.cursor.execute("CREATE TABLE Working  (sin TEXT, startShift TEXT, endShift TEXT)")
+        self.cursor.execute("CREATE TABLE Venue   (name TEXT, address TEXT)")
+        self.cursor.execute("CREATE TABLE Sport (name TEXT, sportID INT)")
+        # Note that I am using the start and end time as string. we initially had it as int.
+        self.cursor.execute("CREATE TABLE Session  (start_time TEXT, end_time TEXT, session_id UNSIGNED INT, sport_id UNSIGNED INT, venue_name TEXT, results TEXT)")
+        self.cursor.execute("CREATE TABLE Player (name TEXT , student_num UNSIGNED INT, password TEXT, salt TEXT)")
+        self.cursor.execute("CREATE TABLE PlaysIn (student_num UNSIGNED INT, team_id UNSIGNED INT)")
+        self.cursor.execute("CREATE TABLE Team_ParticipatesIn (name TEXT, team_ID UNSIGNED INT, number_of_players UNSIGNED INT,"
+                       "sport_id UNSIGNED INT, session_id INT,venue_name TEXT)")
 
     # Below are the functions used to update each table of the database for the schemas we defined.
     # Function names and attributes are self-explanatory.
@@ -38,14 +37,14 @@ class SQLAPI(object):
     def insertEmployeesData(self,Sin,fName, lName,username, password,salt ):
         if (isinstance(Sin,str)==True and isinstance(fName,str)==True and isinstance(lName,str)==True
             and isinstance(username,str)==True and isinstance(password,str)==True and isinstance(salt,str)==True):
-            self.c.execute("INSERT INTO Employees VALUES (? ,? ,? ,? ,? ,?)",(Sin,fName,lName,username,password,salt))
+            self.cursor.execute("INSERT INTO Employees VALUES (? ,? ,? ,? ,? ,?)",(Sin,fName,lName,username,password,salt))
             self.conn.commit()
         else:
             print("Insertion for Employee table failed. Types doesn't match.")
 
     def insertWorkingData(self,Sin,start_shift, end_shift ):
         if (isinstance(Sin,str)==True and isinstance(start_shift,str)==True and isinstance(end_shift,str)==True):
-            self.c.execute("INSERT INTO Working VALUES (? ,? ,?)",Sin,start_shift,end_shift)
+            self.cursor.execute("INSERT INTO Working VALUES (? ,? ,?)",Sin,start_shift,end_shift)
             self.conn.commit()
         else:
             print("Insertion for Working table failed. Types doesn't match.")
@@ -56,7 +55,7 @@ class SQLAPI(object):
         elif (isinstance(Address,str)==False ):
             print("Inserting Address for Venue table failed. Types doesn't match.")
         else:
-            self.c.execute("INSERT INTO Venue VALUES (? ,?)",(Name,Address))
+            self.cursor.execute("INSERT INTO Venue VALUES (? ,?)",(Name,Address))
             self.conn.commit()
 
     def insertSportData (self, sportName, sportID):
@@ -65,14 +64,14 @@ class SQLAPI(object):
         elif (isinstance(sportID,int)==False or sportID<0):
             print("Inserting Sport ID for Sport table failed. Type doesn't match.")
         else:
-            self.c.execute("INSERT INTO Sport VALUES (? ,?)",(sportName,sportID))
+            self.cursor.execute("INSERT INTO Sport VALUES (? ,?)",(sportName,sportID))
             self.conn.commit()
 
     def insertSessionData (self, startTime, endTime, sessionID, sportID,venueName,results):
         """Note: we had start time and end time as int but i am using it as string here"""
         if (isinstance(startTime,str)==True and isinstance(endTime,str)==True and isinstance(sessionID,int)==True
         and isinstance(sportID,int) and sportID>=0 and isinstance(venueName,str)==True and isinstance(results,str)):
-            self.c.execute("INSERT INTO Session VALUES (? ,?,? ,?,? ,?)",(startTime,endTime,sessionID,sportID,venueName,results))
+            self.cursor.execute("INSERT INTO Session VALUES (? ,?,? ,?,? ,?)",(startTime,endTime,sessionID,sportID,venueName,results))
             self.conn.commit()
         else:
             print("Insertion for Session table failed. Types doesn't match.")
@@ -80,7 +79,7 @@ class SQLAPI(object):
     def insertPlayerData (self, name, studentID, password, salt):
         if (isinstance(name,str)==True and isinstance(password,str)==True
         and isinstance(studentID,int) and studentID>=0 and isinstance(salt,str)==True ):
-            self.c.execute("INSERT INTO Player VALUES (?,?,?,?)",(name,password,studentID,salt))
+            self.cursor.execute("INSERT INTO Player VALUES (?,?,?,?)",(name,password,studentID,salt))
             self.conn.commit()
         else:
             print("Insertion for Player table failed. Types doesn't match.")
@@ -91,7 +90,7 @@ class SQLAPI(object):
         elif (isinstance(teamID,int)==False or teamID<0):
             print("Inserting team ID for PlaysIn table failed. Type doesn't match.")
         else:
-            self.c.execute("INSERT INTO PlaysIn VALUES (? ,?)",(studentNum,teamID))
+            self.cursor.execute("INSERT INTO PlaysIn VALUES (? ,?)",(studentNum,teamID))
             self.conn.commit()
 
     def insertTeamParInData(self,teamName,teamID,numPlayers,sportID,sessionID,venueName):
@@ -99,7 +98,7 @@ class SQLAPI(object):
         if (isinstance(teamName,str)==True and isinstance(teamID,int)==True and teamID>=0
             and isinstance(sessionID,int)==True and isinstance(sportID,int) and sportID>=0
             and isinstance(venueName,str)==True and isinstance(numPlayers,int) and numPlayers>0):
-            self.c.execute("INSERT INTO TeamParIn VALUES (?,?,?,?,?,?)",(teamName,teamID,numPlayers,sportID,sessionID,venueName))
+            self.cursor.execute("INSERT INTO TeamParIn VALUES (?,?,?,?,?,?)",(teamName,teamID,numPlayers,sportID,sessionID,venueName))
             self.conn.commit()
         else:
             print("Insertion for Team_ParticipatesIn table failed. Types doesn't match.")
