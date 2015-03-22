a33f6c0e21f01bfcc4e2f0c79311ab784d376d48
Ehsan70 <a.ehsan70@gmail.com>
2015-03-21 18:43:35 -0700

    Added documentation

--- sqlAPI.py
+++ sqlAPI.py
@@ -3,19 +3,23 @@ import sqlite3
 
 class SQLAPI(object):
 
-    def __init__(self, DBaddress='project.db'):
+    def __init__(self, DB_address='project.db'):
         """
-        input : String containing address of the database
-        output: the cursor for that database for later use
+
+        :rtype : object
+        :type DB_address: str
+        :param DB_address: String containing address of the database
+        :return : the cursor for that database for later use
         """
-        if isinstance(DBaddress,str):
-            self.conn = sqlite3.connect(DBaddress)
+        if isinstance(DB_address,str):
+            self.conn = sqlite3.connect(DB_address)
             self.cursor = self.conn.cursor()
         else :
             print("Address of the database is not string/valid")
 
     def tableCreate(self):
         """
+        :rtype : object
         Note: This function is creates the tables and stuff but does not properly set the foreign and primary keys.
         We only need to do this one and therefore we can do it using SQL manager once and use it for the rest of time
         So use this for testing only.
@@ -35,6 +39,21 @@ class SQLAPI(object):
     # Function names and attributes are self-explanatory.
 
     def insertEmployeesData(self,Sin,fName, lName,username, password,salt ):
+        """
+        :type Sin: str
+        :param Sin: Employees SIN number
+        :type fName: str
+        :param fName: First name
+        :type lName:str
+        :param lName: Last name
+        :type username: str
+        :param username: Username for employee
+        :type password: str
+        :param password: Password for that employee
+        :type salt: str
+        :param salt: salt for fun :D
+        :raise TypeError: if the above type doesnt match it will throw an exception
+        """
         if (isinstance(Sin,str)==True and isinstance(fName,str)==True and isinstance(lName,str)==True
             and isinstance(username,str)==True and isinstance(password,str)==True and isinstance(salt,str)==True):
             self.cursor.execute("INSERT INTO Employees VALUES (? ,? ,? ,? ,? ,?)",(Sin,fName,lName,username,password,salt))
@@ -44,6 +63,14 @@ class SQLAPI(object):
             #print("Insertion for Employee table failed. Types doesn't match.")
 
     def insertWorkingData(self,Sin,start_shift, end_shift ):
+        """
+        :type Sin: str
+        :param Sin: Employees SIN number
+        :type start_shift: str
+        :param start_shift: Employees start shift
+        :type end_shift: str
+        :param end_shift: Employees end shift
+        """
         if (isinstance(Sin,str)==True and isinstance(start_shift,str)==True and isinstance(end_shift,str)==True):
             self.cursor.execute("INSERT INTO Working VALUES (? ,? ,?)",Sin,start_shift,end_shift)
             self.conn.commit()
@@ -52,6 +79,12 @@ class SQLAPI(object):
             #print("Insertion for Working table failed. Types doesn't match.")
 
     def insertVenueData(self,Name,Address):
+        """
+        :type Name: str
+        :param Name: Venue name
+        :type Address: str
+        :param Address: Address of venue
+        """
         if (isinstance(Name,str)==False) :
             raise TypeError("Name type must be string.")
             #print("Inserting Name for Venue table failed. Types doesn't match.")
@@ -63,6 +96,13 @@ class SQLAPI(object):
             self.conn.commit()
 
     def insertSportData (self, sportName, sportID):
+        """
+
+        :type sportName: str
+        :param sportName: Name of the sport
+        :type sportID: int
+        :param sportID: ID of the sport type
+        """
         if (isinstance(sportName,str)==False):
             raise TypeError("sportName type must be string.")
             #print("Inserting Sport Name for Sport table failed. Type doesn't match.")
@@ -74,6 +114,21 @@ class SQLAPI(object):
             self.conn.commit()
 
     def insertSessionData (self, startTime, endTime, sessionID, sportID,venueName,results):
+        """
+
+        :type startTime: str
+        :param startTime: Start time for a session. The time when session starts
+        :type endTime: str
+        :param endTime: End time for a session. The time when session ends
+        :type sessionID: int
+        :param sessionID: ID for the drop-in session
+        :type sportID: int
+        :param sportID: ID for the sport type
+        :type venueName: str
+        :param venueName: Name of the venue
+        :type results: str
+        :param results: Result of the game (not sure)
+        """
         """Note: we had start time and end time as int but i am using it as string here"""
         if (isinstance(startTime,str)==True and isinstance(endTime,str)==True and isinstance(sessionID,int)==True
         and isinstance(sportID,int) and sportID>=0 and isinstance(venueName,str)==True and isinstance(results,str)):
@@ -84,6 +139,16 @@ class SQLAPI(object):
             #print("Insertion for Session table failed. Types doesn't match.")
 
     def insertPlayerData (self, name, studentID, password, salt):
+        """
+        :type name: str
+        :param name: Name of the student/player
+        :type studentID:int
+        :param studentID: Student ID of the player
+        :type password: str
+        :param password: Password for that student
+        :type salt: str
+        :param salt: I dont know
+        """
         if (isinstance(name,str)==True and isinstance(password,str)==True
         and isinstance(studentID,int) and studentID>=0 and isinstance(salt,str)==True ):
             self.cursor.execute("INSERT INTO Player VALUES (?,?,?,?)",(name,password,studentID,salt))
@@ -93,6 +158,12 @@ class SQLAPI(object):
             #print("Insertion for Player table failed. Types doesn't match.")
 
     def insertPlaysInData (self, studentNum, teamID):
+        """
+        :type studentNum int
+        :param studentNum: Student number of the player
+        :type teamID: int
+        :param teamID: Team ID
+        """
         if (isinstance(studentNum,str)==False):
             raise TypeError("Student number  type must be string.")
             #print("Inserting student Number for PlaysIn table failed. Type doesn't match.")
@@ -104,6 +175,21 @@ class SQLAPI(object):
             self.conn.commit()
 
     def insertTeamParInData(self,teamName,teamID,numPlayers,sportID,sessionID,venueName):
+        """
+
+        :type teamName: str
+        :param teamName: Name of the team
+        :type teamID: ID for team
+        :param teamID: ID for team
+        :type numPlayers: int
+        :param numPlayers: Number of player in a team
+        :type sportID: int
+        :param sportID: ID for sport
+        :type sessionID: int
+        :param sessionID: ID for session
+        :type venueName: str
+        :param venueName: Name of the venue where the drop-in session is held
+        """
         """Inserting data for Team_ParticipatesIn table """
         if (isinstance(teamName,str)==True and isinstance(teamID,int)==True and teamID>=0
             and isinstance(sessionID,int)==True and isinstance(sportID,int) and sportID>=0
