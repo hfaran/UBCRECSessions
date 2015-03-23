beeaa725ffa0adf41bbf4b032f0a98091e5fadc7
hfaran <hamzafaran@outlook.com>
2015-03-23 01:57:14 -0700

    Fix map; track API docs

--- src/ubcrec/routes.py
+++ src/ubcrec/routes.py
@@ -8,8 +8,8 @@ def assemble_routes():
     """Assembles all routes and returns"""
     # custom_routes = [("/", views.signin.Landing)]  # TODO
     api_routes = get_routes(api)
-    view_routes = map(
+    view_routes = list(map(
         lambda r: (r[0].replace("/views", "", 1), r[1]),
         get_routes(views)  # View routes with /views removed
-    )
+    ))
     return api_routes + view_routes  # + custom_routes  # TODO
