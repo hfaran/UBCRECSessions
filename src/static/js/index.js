c1bea7d264b16901f4c395758b1dd6a28d56013e
Dylan Dumesnil <dadumesnil@gmail.com>
2015-03-24 13:43:51 -0700

    Started on AJAX for /api/session/sessions/ waiting on Ehsan to implement sqlAPI. This is a good example of AJAX for Alan to learn from

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -14,4 +14,75 @@ $( document ).ready(function() {
 	$('#end-day').datepicker('setDate', date7DaysFromNow);
 	$('#start-day').datepicker('update');
 	$('#end-day').datepicker('update');
+
+	loadSessions();
+	// @TODO Load Sports options for search
+	// @TODO Load Venues options for search
+
+	// Add the loadSessions method to the 'Search Sessions' button
+	$("#search-sessions").on("click", loadSessions);
 });
+
+// This function builds the sessionsQueryData object and sends the AJAX call
+function loadSessions() {
+	// This data structure is sent to the API when searching for queries
+	// Empty venues or sports will return all venues or sports
+	// started_after and ended_before is UNIX timestamp
+	var sessionsQueryData = {
+		"venues" : [],
+		"sports" : [],
+		"started_after" : 0,
+		"ended_before" : 0
+	}
+
+	// Clear the venue list
+	sessionsQueryData["venues"] = [];
+	// Add all selected venues to the venue list
+	$("#venues option").each(function() {
+		// Check if this venue is selected
+		if($(this).is(':selected')) {
+			sessionsQueryData["venues"].push($(this).val());
+		}
+	});
+
+	// Clear the sport list
+	sessionsQueryData["sports"] = [];
+	// Add all selected venues to the sport list
+	$("#sports option").each(function() {
+		// Check if this sport is selected
+		if($(this).is(':selected')) {
+			sessionsQueryData["sports"].push($(this).val());
+		}
+	});
+
+	// Get the start date
+	var startDate = $('#start-day').datepicker('getDate');
+	// Assign it within our object
+	// JavaScript getTime is UNIX time in milliseconds, API wants seconds
+	sessionsQueryData["started_after"] = startDate.getTime() / 1000;
+
+	// Get the end date
+	var endDate = $('#end-day').datepicker('getDate');
+	// Assign it within our object
+	// JavaScript getTime is UNIX time in milliseconds, API wants seconds
+	sessionsQueryData["ended_before"] = endDate.getTime() / 1000;
+
+	// Print it for debugging
+	console.log(sessionsQueryData);
+
+	// Perform the AJAX HTTP request
+	// We're using a POST here which is bad practice when GETting data
+	// But for the sake of time, we're using POST
+	$.ajax({
+		url : "/api/session/sessions/",
+		type : "POST",
+		data : JSON.stringify(sessionsQueryData),
+		success : loadSessionsSuccess,
+		dataType : "json"
+	});
+}
+
+function loadSessionsSuccess(data) {
+	// @TODO Generated rows for each sessions
+	console.log(data);
+}
