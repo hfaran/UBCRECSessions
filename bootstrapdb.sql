5ae4453f3b676613a96a7296bc3a6302d153f209
hfaran <hamzafaran@outlook.com>
2015-03-24 17:27:22 -0700

    Do REFERENCES properly

--- bootstrapdb.sql
+++ bootstrapdb.sql
@@ -15,7 +15,7 @@ CREATE TABLE Working (
   start_shift INTEGER,
   end_shift   INTEGER,
   PRIMARY KEY (sin, start_shift, end_shift),
-  FOREIGN KEY (sin) REFERENCES Employees
+  FOREIGN KEY (sin) REFERENCES Employees (sin)
 );
 CREATE TABLE Venue (
   name    TEXT PRIMARY KEY,
@@ -33,8 +33,8 @@ CREATE TABLE Sessions (
   sport_id   INTEGER,
   venue_name TEXT,
   results    TEXT,
-  FOREIGN KEY (sport_id) REFERENCES Sport,
-  FOREIGN KEY (venue_name) REFERENCES Venue
+  FOREIGN KEY (sport_id) REFERENCES Sport (sport_id),
+  FOREIGN KEY (venue_name) REFERENCES Venue (venue_name)
 );
 CREATE TABLE Players (
   name        TEXT,
@@ -46,8 +46,8 @@ CREATE TABLE PlaysIn (
   student_num INTEGER,
   team_id     INTEGER,
   PRIMARY KEY (student_num, team_id),
-  FOREIGN KEY (student_num) REFERENCES Players,
-  FOREIGN KEY (team_id) REFERENCES Team_ParticipatesIn
+  FOREIGN KEY (student_num) REFERENCES Players (student_num),
+  FOREIGN KEY (team_id) REFERENCES Team_ParticipatesIn (team_id)
 );
 CREATE TABLE Team_ParticipatesIn (
   team_id           INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -56,9 +56,9 @@ CREATE TABLE Team_ParticipatesIn (
   sport_id          INTEGER,
   session_id        INT,
   venue_name        TEXT,
-  FOREIGN KEY (session_id) REFERENCES Sessions,
-  FOREIGN KEY (sport_id) REFERENCES Sport,
-  FOREIGN KEY (venue_name) REFERENCES Venue
+  FOREIGN KEY (session_id) REFERENCES Sessions (session_id),
+  FOREIGN KEY (sport_id) REFERENCES Sport (sport_id),
+  FOREIGN KEY (venue_name) REFERENCES Venue (venue_name)
 );
 
 
